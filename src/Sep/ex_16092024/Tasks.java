0232907275414a4aa622dff324c344825ad2032e
SriSarika-K <srisarika.kokkireni@gmail.com>
2024-10-07 17:44:57 +0530

    Add files

--- src/Sep/ex_16092024/Tasks.java
+++ src/Sep/ex_16092024/Tasks.java
@@ -12,28 +12,17 @@ public class Tasks {
         // 1 byte & 8 bits
         long l = 10;
         // 8 byte & 64 bits
-        String s = "sarika";
-        // 8 byte & 64 bits
 
-        System.out.println(s);
-        System.out.println(l);
-        System.out.println(b);
 
         // 3. Another byte b = 10; byte c = 10;
         // What is the answer if we perform b+c, What is the data type it will give in result.
-        byte b = 10;
-        byte c = 10;
-        int a = b+c;
-        System.out.printf("%d+%d=%d",b,c,a);
+        byte b1 = 10;
+        byte c1 = 10;
+        int a = b1+c1;
         System.out.println(a);
         //As per java rules Byte + Byte = int,
         //so if Byte b = 10 and Byte c = 10 then the answer 20 is stored in int.
 
-        // Extra info
-        char x = 'A';
-        char y = 'B';
-        int z = x+y;
-        System.out.println(z);
 
        //4
         short s = 10;
